c7430be5964bbb244dc81a4ae5c44513bc2b16b2
Prime-DS <prime.up17@gmail.com>
2022-08-21 14:17:50 +0000

    Deploying to gh-pages from @ Prime-DS/goit-js-hw-11@ffe668f85458b3c887643e1805cc7ee0d7f02295 🚀

--- index.7decee32.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#vimeo-player"),t=new Player(e);t.on("timeupdate",throttle((function(e){localStorage.setItem("videoplayer-current-time",e.seconds),console.log(e.seconds)}),1e3));const o=localStorage.getItem("videoplayer-current-time");o&&t.setCurrentTime(o);
-//# sourceMappingURL=index.7decee32.js.map
